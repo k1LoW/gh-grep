024240971eb28262f8c8a522396f55384404df3c
k1LoW <k1lowxb@gmail.com>
2021-11-01 14:31:44 +0900

    Color matched word

---
+++ internal/color.go
@@ -0,0 +1,28 @@
+package internal
+
+import (
+	"unicode/utf8"
+
+	"github.com/fatih/color"
+)
+
+func PrintLine(line string, matches [][]int, c *color.Color) string {
+	if !utf8.Valid([]byte(line)) {
+		return line
+	}
+	if len(matches) == 0 {
+		return line
+	}
+	pos := 0
+	colored := ""
+	for _, m := range matches {
+		if pos < m[0] {
+			colored += line[pos:m[0]]
+		}
+		colored += c.Sprint(line[m[0]:m[1]])
+		pos = m[1]
+	}
+	colored += line[pos:]
+
+	return colored
+}
