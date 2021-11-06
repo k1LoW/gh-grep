d6262efa6410a17dee0cedfc9a958858a9d8d7aa
k1LoW <k1lowxb@gmail.com>
2021-11-06 12:30:57 +0900

    Use github.com/k1LoW/go-github-client/v39/factory

--- gh/gh.go
+++ gh/gh.go
@@ -3,15 +3,9 @@ package gh
 import (
 	"context"
 	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
-	"time"
 
 	"github.com/google/go-github/v39/github"
+	"github.com/k1LoW/go-github-client/v39/factory"
 )
 
 type Gh struct {
@@ -19,48 +13,12 @@ type Gh struct {
 }
 
 func New() (*Gh, error) {
-
-	var token, v3ep string
-	if EnvsNotEmpty("CI", "GITHUB_ACTION", "GITHUB_API_URL", "GITHUB_TOKEN") {
-		log.Println("Run on GitHub Actions")
-		// GitHub Actions
-		token = os.Getenv("GITHUB_TOKEN")
-		v3ep = os.Getenv("GITHUB_API_URL")
-	} else if os.Getenv("GH_HOST") != "github.com" && (EnvsNotEmpty("GH_HOST", "GH_ENTERPRISE_TOKEN") || EnvsNotEmpty("GH_HOST", "GITHUB_ENTERPRISE_TOKEN")) {
-		// GitHub Enterprise Server
-		log.Println("Run in GitHub Enterprise Server")
-		token = os.Getenv("GH_ENTERPRISE_TOKEN")
-		if token == "" {
-			token = os.Getenv("GITHUB_ENTERPRISE_TOKEN")
-		}
-		v3ep = fmt.Sprintf("https://%s/api/v3", os.Getenv("GH_HOST"))
-	} else {
-		log.Println("Run in GitHub.com")
-		// GitHub.com
-		token = os.Getenv("GH_TOKEN")
-		if token == "" {
-			token = os.Getenv("GITHUB_TOKEN")
-		}
+	client, err := factory.NewGithubClient()
+	if err != nil {
+		return nil, err
 	}
-
-	if token == "" {
-		return nil, fmt.Errorf("env %s is not set", "GITHUB_TOKEN")
-	}
-
-	v3c := github.NewClient(httpClient(token))
-	if v3ep != "" {
-		baseEndpoint, err := url.Parse(v3ep)
-		if err != nil {
-			return nil, err
-		}
-		if !strings.HasSuffix(baseEndpoint.Path, "/") {
-			baseEndpoint.Path += "/"
-		}
-		v3c.BaseURL = baseEndpoint
-	}
-
 	return &Gh{
-		client: v3c,
+		client: client,
 	}, nil
 }
 
@@ -131,40 +89,4 @@ func (g *Gh) ContentURL(ctx context.Context, owner, repo, path string) (string, 
 		return "", fmt.Errorf("%s is not file", path)
 	}
 	return fc.GetHTMLURL(), nil
-}
-
-func EnvsNotEmpty(keys ...string) bool {
-	for _, k := range keys {
-		if os.Getenv(k) == "" {
-			return false
-		}
-	}
-	return true
-}
-
-type roundTripper struct {
-	transport   *http.Transport
-	accessToken string
-}
-
-func (rt roundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	r.Header.Set("Authorization", fmt.Sprintf("token %s", rt.accessToken))
-	return rt.transport.RoundTrip(r)
-}
-
-func httpClient(token string) *http.Client {
-	t := &http.Transport{
-		Dial: (&net.Dialer{
-			Timeout: 5 * time.Second,
-		}).Dial,
-		TLSHandshakeTimeout: 5 * time.Second,
-	}
-	rt := roundTripper{
-		transport:   t,
-		accessToken: token,
-	}
-	return &http.Client{
-		Timeout:   time.Second * 30,
-		Transport: rt,
-	}
 }
